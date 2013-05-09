1525c80cfcc5171c0a913b1e94125c005a983331
Gary Davison <gary.davison@jhu.edu>
2013-05-09 04:24:54 -0400

    Add a static pages controller.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '759b45db4698a92bd042b03aec10adbc6b5f433e9677d8d8ec9879944ca526bd40ea5c1931bf7a5e9f6e2c2d0dca70aaccd0973ee0682baae4f68f51568c743a'
+SampleApp::Application.config.secret_token = 'd3fbfb7779ff12d0ba7c9830215bdaa785a72dd188d07419b0b77b7ca68a5e07a3ab4640ea52b554ae62c056cc7d13170bb9385d5719e6b5d23e264bd4e2d7e6'
